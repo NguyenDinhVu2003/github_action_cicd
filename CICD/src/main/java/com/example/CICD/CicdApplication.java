6b3e011ce134d86ef1e7ead2bbaabb185676b05a
nguyendinhvu20215257@gmail.com <nguyendinhvu20215257@gmail.com>
2025-09-04 21:47:17 +0700

    code

---
+++ CICD/src/main/java/com/example/CICD/CicdApplication.java
@@ -0,0 +1,18 @@
+package com.example.CICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+
+	public String welcome() {
+		return "Welcome to CICD";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
